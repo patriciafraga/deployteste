ef270410871063d8ae9590d90acaa68126f75e91
patriciafraga <patricia.f.fraga@gmail.com>
2023-11-07 21:36:19 -0300

    porta = port

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 const express = require('express');
 
 const app = express();
-const porta = process.env.PORTA || 3000;
+const porta = process.env.PORT || 3000;
 const rota = require('./rotas/rota');
 app.use(rota);
 app.use(express.json());
 
-app.listen(porta, () => console.log(`Rodando em http://localhost:${porta}`));
+app.listen(porta, () => console.log(`Rodando em https://https-patriciafraga-github-io-deployteste.onrender.com/${porta}`));
